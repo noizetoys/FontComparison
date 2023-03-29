1b4c7020b7c62077a710d4a1642913b61c154eef
James Majors <noizetoys@yahoo.com>
2023-03-29 16:17:29 -0400

    Initial Commit

---
+++ FontComparison/FontComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontComparisonApp.swift
+//  FontComparison
+//
+//  Created by Apple User on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FontComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
